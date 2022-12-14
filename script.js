8591ac7e7e8c1ad4b597dd165715c9a6e7da82fd
OkumuP <115947599+OkumuP@users.noreply.github.com>
2022-12-14 18:24:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.getElementById("driver").onclick = function(){
+//     prompt ('Enter your name')
+// }
+let btn = document.getElementById("driver");
+let register = document.getElementById("register")
+
+btn.addEventListener("click", () =>{
+    if(register.style.display === "none"){
+        register.style.display = "block";
+    }else {
+        register.style.display = "none";
+    }
+})
